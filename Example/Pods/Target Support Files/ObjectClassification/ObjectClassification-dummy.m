72f9e19af85b697ef76206d02d5799d362bf0a31
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2021-01-15 16:39:10 -0300

    Criação Pod

---
+++ Example/Pods/Target Support Files/ObjectClassification/ObjectClassification-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ObjectClassification : NSObject
+@end
+@implementation PodsDummy_ObjectClassification
+@end
---
+++ Example/Pods/Target Support Files/Pods-ObjectClassification_Example/Pods-ObjectClassification_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ObjectClassification_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ObjectClassification_Example
+@end
